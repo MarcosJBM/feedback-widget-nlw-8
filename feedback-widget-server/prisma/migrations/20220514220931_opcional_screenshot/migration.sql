ccb1d760a3aa834af7c43d9901d6e4f0126b43ba
Marcos Botene <marcosjbotene@gmail.com>
2022-05-14 19:12:00 -0300

    route to create a feedback.

---
+++ feedback-widget-server/prisma/migrations/20220514220931_opcional_screenshot/migration.sql
@@ -0,0 +1,13 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_feedbacks" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "type" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
+    "screenshot" TEXT
+);
+INSERT INTO "new_feedbacks" ("comment", "id", "screenshot", "type") SELECT "comment", "id", "screenshot", "type" FROM "feedbacks";
+DROP TABLE "feedbacks";
+ALTER TABLE "new_feedbacks" RENAME TO "feedbacks";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- feedback-widget-server/src/server.ts
+++ feedback-widget-server/src/server.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (request: Request, response: Response) => {
-  return response.send('Hello World!');
+app.use(express.json());
+
+app.post('/feedbacks', async (request: Request, response: Response) => {
+  const { type, comment, screenshot } = request.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return response.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
