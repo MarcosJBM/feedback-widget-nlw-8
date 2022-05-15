2a43b2632ebef49da14a361a34a85988ede02ab9
Marcos Botene <marcosjbotene@gmail.com>
2022-05-15 14:18:02 -0300

    added expo fonts.

--- feedback-widget-mobile/App.tsx
+++ feedback-widget-mobile/App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Widget } from './src/components/widget';
+import { theme } from './src/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}>
+      <Widget />
+
+      <StatusBar style='light' backgroundColor='transparent' translucent />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ feedback-widget-mobile/src/components/widget/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Widget() {
+  return <View style={styles.container}></View>;
+}
