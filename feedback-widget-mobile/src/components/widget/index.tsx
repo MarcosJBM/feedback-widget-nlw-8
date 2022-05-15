eaab0f4ad051af09acc267496e9116a127503eee
Marcos Botene <marcosjbotene@gmail.com>
2022-05-15 15:18:46 -0300

    manipulating the widget with gestures.

--- feedback-widget-mobile/src/components/widget/index.tsx
+++ feedback-widget-mobile/src/components/widget/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef } from 'react';
+import { ChatTeardropDots } from 'phosphor-react-native';
+import { TouchableOpacity } from 'react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
+import { theme } from '../../theme';
 import { styles } from './styles';
 
-export function Widget() {
-  return <View style={styles.container}></View>;
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
+
+  return (
+    <>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
+        <ChatTeardropDots
+          size={24}
+          weight='bold'
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}></BottomSheet>
+    </>
+  );
 }
+
+export default gestureHandlerRootHOC(Widget);
