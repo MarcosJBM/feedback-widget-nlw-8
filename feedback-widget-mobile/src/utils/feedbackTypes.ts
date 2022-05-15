fc1a9a63ebb3841d02365e0dc29e4493b85bb2b7
Marcos Botene <marcosjbotene@gmail.com>
2022-05-15 14:09:36 -0300

    created feedback types.

---
+++ feedback-widget-mobile/src/utils/feedbackTypes.ts
@@ -0,0 +1,14 @@
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: require('../assets/bug.png'),
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: require('../assets/idea.png'),
+  },
+  OTHER: {
+    title: 'Outro',
+    image: require('../assets/thought.png'),
+  },
+};
