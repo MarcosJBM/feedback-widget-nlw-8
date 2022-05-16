3d32837958e590c4e34bffb3a30ae617f9a02314
Marcos Botene <marcosjbotene@gmail.com>
2022-05-15 22:19:34 -0300

    if the port is not available, use port 3333.

--- feedback-widget-server/src/server.ts
+++ feedback-widget-server/src/server.ts
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('HTTP Server Running!');
 });
